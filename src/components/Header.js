35a7cdbfb42bd86befb0b24f92fbf5022585b0aa
Ryu Tatsuki <ryutatsuki@ryuguitar.jp>
2022-01-28 20:42:30 +0900

    inline styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
   return (
     <header>
-      <h1>{title}</h1>
+      {/* use {{}} for inline styling */}
+      {/* different from regular HTML, camel-case attributes is used with JSX */}
+      <h1 style={{ color: 'red', backgroundColor: 'black' }}>{title}</h1>
     </header>
   );
 }
