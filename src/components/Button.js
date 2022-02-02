5390f32ca6d9ce35e7d9acf8016e75b69a13545c
Ryu Tatsuki <ryutatsuki@ryuguitar.jp>
2022-02-03 01:08:47 +0900

    created Tasks.js component with dummy data

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,8 @@
 import PropTypes from 'prop-types';
 
-// onClick props is caught up here from Header.js
 const Button = ({ color, text, onClick }) => {
   return (
     <button
-      // call onClick props and sent up to Header.js
       onClick={onClick}
       style={{ backgroundColor: color }}
       className='btn'
@@ -21,7 +19,6 @@ Button.defaultProps = {
 Button.propTypes = {
   text: PropTypes.string,
   color: PropTypes.string,
-  // added PropType for onClick
   onClick: PropTypes.func
 }
 
