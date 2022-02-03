69d0a769924f81a48181e9c5ea0806f4a3de0021
Ryu Tatsuki <ryutatsuki@ryuguitar.jp>
2022-02-04 02:26:33 +0900

    installed react-icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,6 @@
-// pass in task as a prop
 const Task = ({ task }) => {
   return (
     <div className='task'>
-      {/* use of prop rather than just hard coded text */}
       <h3>{task.text}</h3>
       <p>{task.day}</p>
     </div>
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,9 @@
-// import each task form Task.js component
 import Task from "./Task";
 
 const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
-        // key prop is necessary
-        // pass in task as a prop
         <Task key={task.id} task={task} />
       ))}
     </>
