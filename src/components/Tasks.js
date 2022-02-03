8868ada8ddecf72883b34b37770f272d88c79445
Ryu Tatsuki <ryutatsuki@ryuguitar.jp>
2022-02-04 01:32:21 +0900

    State and useState Hook

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,33 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Doctors Appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true
-  },
-  {
-    id: 2,
-    text: 'Meeting at School',
-    day: 'Feb 6th at 1:30pm',
-    reminder: true
-  },
-  {
-    id: 3,
-    text: 'Food Shopping',
-    day: 'Feb 5th at 2:30pm',
-    reminder: false
-  }
-]
+import { useState } from "react";
 
 const Tasks = () => {
+  // rather than getting data from outside of component,
+  // the data is now a part of this component state.
+  // if you wanna change any part of state, you will wanna use setTasks.
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false
+    }
+  ]);
+
   return (
+    // things like tasks.push() cannot be used since state is immutable
+    // setTasks([...tasks, {}]) can be used by creating new object
     <>
       {/* map() to get text for each one from [] above and output that */}
       {tasks.map((task) => (
