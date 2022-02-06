8f168ab1cfd5f7708f1644b43821fb165ddfd7f7
Ryu Tatsuki <ryutatsuki@ryuguitar.jp>
2022-02-07 02:34:40 +0900

    import react-icons/fa and add delete button

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,13 @@
+// bring in FontAwesome from react-icons
+import { FaTimes } from 'react-icons/fa';
+
 const Task = ({ task }) => {
   return (
     <div className='task'>
-      <h3>{task.text}</h3>
+      <h3>
+        {/* added delete button with inline style */}
+        {task.text} <FaTimes style={{ color: 'red', cursor: 'pointer' }} />
+      </h3>
       <p>{task.day}</p>
     </div>
   );
