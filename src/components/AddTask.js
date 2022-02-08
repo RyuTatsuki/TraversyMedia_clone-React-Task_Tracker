25273893c0de1c7b9624a5dd8469ab5e0775c616
Ryu Tatsuki <ryutatsuki@ryuguitar.jp>
2022-02-08 22:04:16 +0900

    form input state (controlled components)

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,17 +1,40 @@
+// each inputs have its own component level state (NOT app level state)
+// make this component level state controllable with useState
+import { useState } from "react";
+
 const AddTask = () => {
+  // set default state of each inputs
+  const [text, setText] = useState('');
+  const [day, setDay] = useState('');
+  const [reminder, setReminder] = useState(false);
+
   return (
     <form className='add-form'>
       <div className='form-control'>
         <label>Task</label>
-        <input type='text' placeholder='Add Task' />
+        <input
+          type='text'
+          placeholder='Add Task'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
       <div className='form-control'>
         <label>Day & Time</label>
-        <input type='text' placeholder='Day & Time' />
+        <input
+          type='text'
+          placeholder='Day & Time'
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        />
       </div>
       <div className='form-control form-control-check'>
         <label>Set Reminder</label>
-        <input type='checkbox' />
+        <input
+          type='checkbox'
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
       </div>
 
       <input type="submit" value='Save Task' className='btn btn-block' />
