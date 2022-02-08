683da311540afe45152286d12b3b51da317eb2ef
Ryu Tatsuki <30724865+RyuTatsuki@users.noreply.github.com>
2022-02-08 23:11:01 +0900

    Revert "Revert "Add task form""

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+// import AddTask component
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -24,6 +26,18 @@ function App() {
     }
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    // generate random id
+    const id = Math.floor(Math.random() * 10000) + 1;
+    // newTask is an object with random id and copy of the task which is passed in
+    const newTask = { id, ...task };
+
+    // set tasks as an array with copy of current tasks and newTask object
+    setTasks([...tasks, newTask]);
+
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -31,14 +45,15 @@ function App() {
 
   // Toggle Reminder
   const toggleReminder = (id) => {
-    // ternary operator
     setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task));
   }
 
   return (
     <div className="container">
       <Header />
-      {/* toggleReminder as onToggle prop */}
+      {/* embed AddTask */}
+      {/* pass addTask function as a prop into AddTask */}
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to Show'}
     </div>
   );
