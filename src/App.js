ff45faa003f467d8572ee569578609d83453ac88
Ryu Tatsuki <ryutatsuki@ryuguitar.jp>
2022-04-17 02:30:14 +0900

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
     return data;
   }
 
-  // get a single task to make update task work
   // Fetch Task
   const fetchTask = async (id) => {
     const res = await fetch(`http://localhost:5000/tasks/${id}`);
@@ -46,17 +45,10 @@ function App() {
     const data = await res.json();
 
     setTasks([...tasks, data]);
-
-    // no need to create an id since it assigns an id automatically
-    // const id = Math.floor(Math.random() * 10000) + 1;
-    // const newTask = { id, ...task };
-
-    // setTasks([...tasks, newTask]);
   }
 
   // Delete Task
   const deleteTask = async (id) => {
-    //just use await don't need to save it in a variable since getting no data back
     await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'DELETE'
     })
@@ -66,11 +58,9 @@ function App() {
 
   // Toggle Reminder
   const toggleReminder = async (id) => {
-    // get a task and then create a new task to put it in a variable
     const taskToToggle = await fetchTask(id);
     const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
 
-    // update a task
     const res = await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'PUT',
       headers: {
@@ -81,7 +71,6 @@ function App() {
 
     const data = await res.json();
 
-    // change "reminder: !task.reminder" to "reminder: data.reminder"
     setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task));
   }
 
