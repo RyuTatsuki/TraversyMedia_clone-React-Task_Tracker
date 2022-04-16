8480275977d3734e65956eb2e5278e661e607d25
Ryu Tatsuki <ryutatsuki@ryuguitar.jp>
2022-04-16 20:16:33 +0900

    made data fetching work

--- src/App.js
+++ src/App.js
@@ -8,16 +8,23 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      const res = await fetch('http://localhost:5000/tasks');
-      const data = await res.json();
-
-      console.log(data);
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
     }
 
-    fetchTasks();
+    getTasks();
     // "dependency array" is empty since nothing is passed in
   }, [])
+
+  // take fetchTasks out from useEffect() since this can be used somewhere else
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+
+    return data;
+  }
 
   // Add Task
   const addTask = (task) => {
