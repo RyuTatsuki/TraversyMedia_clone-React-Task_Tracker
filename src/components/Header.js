0ebab79b95dd32af721d6fda33373ed1e28c086c
Ryu Tatsuki <ryutatsuki@ryuguitar.jp>
2022-04-18 05:28:15 +0900

    hide Button with useLocation when About is shown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,22 @@
 import PropTypes from 'prop-types';
+// useLocation allows to look at the route currently on
+import { useLocation } from 'react-router-dom';
 import Button from './Button';
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button
-        color={showAdd ? 'red' : 'green'}
-        text={showAdd ? 'Close' : 'Add'}
-        onClick={onAdd}
-      />
+      {/* show Button if current location is '/' otherwise not */}
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 }
